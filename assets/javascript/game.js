80dd38d6b645e734ddfdb9478990035f429dce7d
gadcad98 <37885704+gadcad98@users.noreply.github.com>
2018-05-22 18:38:24 -0700

    update jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,15 @@
+function generateCrystals() {
+    for(var i=0; i < 4; i++) {
+     var containerDiv = $("<div>");
+     containerDiv.attr("class", "col-md-1");
+     var imageDiv = $("<img>");
+     imageDiv.attr("src", "./assets/images/spam.jpg");
+     containerDiv.append(imageDiv);
+     $(".crystalHolder").append(containerDiv);
+    }
+}
+
 $("#randomNumber").text(Math.floor((Math.random() * 120) + 1));
+
+generateCrystals();
+
