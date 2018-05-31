083b12f51f55f0ed3e1ac6ad6034b5fd3e591794
gadcad98 <37885704+gadcad98@users.noreply.github.com>
2018-05-30 20:10:51 -0700

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,38 @@
-function generateCrystals() {
-    for(var i=0; i < 4; i++) {
-     var containerDiv = $("<div>");
-     containerDiv.attr("class", "col-md-1");
-     var imageDiv = $("<img>");
-     imageDiv.attr("src", "./assets/images/spam.jpg");
-     containerDiv.append(imageDiv);
-     $(".crystalHolder").append(containerDiv);
-    }
-}
+// Crystal game psuedo code
+// Generate a random number between 19 and 120
+// Create 4 crystals that generate their own random numbers between 1 and 12
+// Each Crystals number is hidden, when the player clicks on a crystal it adds to the players "total score"
+// If the player matches his score to the random number a "win" is added to "win/loss" score
+// If the players score goes over the random number a loss is counted
+// The random numbers reset when the player wins/loses
 
-$("#randomNumber").text(Math.floor((Math.random() * 120) + 1));
+$("#randomNumber").text(Math.floor((Math.random() * 120) + 19));
 
-generateCrystals();
+var crystalNumbers;
+var win;
+var loss;
+
+// for(var i = 0; i < 4; i++){
+//     var crystal = $("<div>");
+//     $(".crystalsHolder").append(crystal);
+
+// } 
+
+$("#crystalHeart").click(function() {
+    // alert("test")
+    
+})
+$("#jade").click(function() {
+    // alert("test")
+    
+})
+$("#opal").click(function() {
+    // alert("test")
+    
+})
+$("#ruby").click(function() {
+    // alert("test")
+    
+})
+
 
